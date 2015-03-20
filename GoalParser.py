0fe7cf56b3a33f34e89b8d9ac61e3dd6a4a4cd12
Brian Stock <bstock92@gmail.com>
2015-03-20 12:30:59 -0400

    Parse csv goals file into a list of individual goals  - TODO: parse individual goals into data structure indicating success and a textual description of the goal

--- GoalParser.py
+++ GoalParser.py
@@ -5,9 +5,15 @@ import csv
 goals_data_file = open('Resources/taskstats_1422603158567.csv')
 goals_data = csv.reader(goals_data_file)
 
-goal_list_list = []
+goal_list = []
 
 for row in goals_data:
-    goal_list_list.append(row[3])
+    # Split rows based on linebreak symbol '#####'
+    goal_row = row[3].split('#####')
 
-print goal_list_list
+    # Remove first element of split row if not meaningful.
+    if goal_row == '' or goal_row == 'undefined':
+        goal_row.pop(0)
+    goal_list = goal_list + goal_row  # Add new row of goals to list of goals.
+
+print goal_list
