ffa227b482c0c9bed4772ceb9ff7758aca13adef
Brian Stock <bstock92@gmail.com>
2015-04-04 12:18:09 -0400

    Implemented counting of top commonly used words for training and test task descriptions  - list of lists: outer is per word, inner is per task  - outputs to MATLAB files  - current number of words used = 200

--- CompliceClassifier.py
+++ CompliceClassifier.py
@@ -1,9 +1,7 @@
 __author__ = 'Brian Stock - bestock@uwaterloo.ca'
 
 from TaskParser import TaskParser
-from TaskParser import TaskParser
 from sklearn.cross_validation import train_test_split
-import neurolab as nl
 from scipy import io
 import numpy as np
 import re
@@ -32,13 +30,26 @@ test_wordcount_data = []
 for task in test_data:
     test_wordcount_data.append(len(re.findall(r'\w+', task[0])))  # count number of words in task description
 
-# Extract number of specific number words used.
+top_words = tp.c.most_common(200)
+# Extract number of specific number words used for top 200 words.
+train_top_wordcounts = []
+test_top_wordcounts = []
+for top_word in top_words:
+    word_string = top_word[0]
+    train_top_wordcounts.append([])
+    for task_description in train_task_descriptions:
+        train_top_wordcounts[-1].append([])
+        train_top_wordcounts[-1][-1] = task_description.count(word_string)
+    test_top_wordcounts.append([])
+    for task_description in test_task_descriptions:
+        test_top_wordcounts[-1].append([])
+        test_top_wordcounts[-1][-1] = task_description.count(word_string)
 
 # Export extracted features to MATLAB data file.
 io.savemat('train_data.mat', {'train_task_descriptions': train_task_descriptions, 'train_task_labels': train_task_labels,
-                              'train_wordcount_data': train_wordcount_data})
+                              'train_wordcount_data': train_wordcount_data, 'train_top_wordcounts': train_top_wordcounts})
 io.savemat('test_data.mat', {'test_task_descriptions': test_task_descriptions, 'test_task_labels': test_task_labels,
-                             'test_wordcount_data': test_wordcount_data})
+                             'test_wordcount_data': test_wordcount_data, 'test_top_wordcounts': test_top_wordcounts})
 
 # Train network with dataset, for number of words in a task.
 #net = nl.net.newff([[min(min(test_wordcount_data), min(train_wordcount_data)), max(max(test_wordcount_data), max(train_wordcount_data))]], [1, 1])
