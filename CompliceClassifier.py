46775f0746417cf35290b0f2f6c8cc8d26a951cd
Malcolm Ocean <malcolm.m.ocean@gmail.com>
2015-04-04 11:25:52 -0400

    best effort feature extractor

--- CompliceClassifier.py
+++ CompliceClassifier.py
@@ -24,23 +24,30 @@ train_data, test_data = train_test_split(task_data, train_size=0.7)
 train_task_descriptions, train_task_labels = zip(*train_data)
 test_task_descriptions, test_task_labels = zip(*test_data)
 
-# Build dataset for number of words in a task.
+# Build dataset for number of words in a task, and presence of best effort words
 train_wordcount_data = []
+train_besteffort_data = []
 for task in train_data:
     train_wordcount_data.append(len(re.findall(r'\w+', task[0])))  # count number of words in task description
+    train_besteffort_data.append(int(bool(re.search('best|try|hardest|attempt|some|a bit', task[0])))) # count best effort works
 test_wordcount_data = []
+test_besteffort_data = []
 for task in test_data:
     test_wordcount_data.append(len(re.findall(r'\w+', task[0])))  # count number of words in task description
+    test_besteffort_data.append(int(bool(re.search('best|try|hardest|attempt|some|a bit', task[0])))) # count best effort works
 
 # Extract number of specific number words used.
 
 # Export extracted features to MATLAB data file.
 io.savemat('train_data.mat', {'train_task_descriptions': train_task_descriptions, 'train_task_labels': train_task_labels,
-                              'train_wordcount_data': train_wordcount_data})
+                              'train_wordcount_data': train_wordcount_data, 'train_besteffort_data': train_besteffort_data})
 io.savemat('test_data.mat', {'test_task_descriptions': test_task_descriptions, 'test_task_labels': test_task_labels,
-                             'test_wordcount_data': test_wordcount_data})
+                             'test_wordcount_data': test_wordcount_data, 'test_besteffort_data': test_besteffort_data})
 
 # Train network with dataset, for number of words in a task.
-#net = nl.net.newff([[min(min(test_wordcount_data), min(train_wordcount_data)), max(max(test_wordcount_data), max(train_wordcount_data))]], [1, 1])
+net = nl.net.newff([[min(min(test_wordcount_data), min(train_wordcount_data), 1), max(max(test_wordcount_data), max(train_wordcount_data))]], [1, 1])
 
-#training_error = net.train(train_wordcount_data, train_task_labels, show=1)
+# print min(train_wordcount_data)
+
+# net.trainf = nl.train.train_bfgs
+training_error = net.train(train_wordcount_data, train_task_labels, show=1)
